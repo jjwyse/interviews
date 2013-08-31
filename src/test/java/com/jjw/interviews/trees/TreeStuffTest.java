f834d1216217b9b3fa1c70ffbd1bc8cfe11fc946
jjwyse <joshua.wyse@gmail.com>
2013-08-31 10:31:26 -0600

    Studying trees

--- src/test/java/com/jjw/interviews/trees/TreeStuffTest.java
+++ src/test/java/com/jjw/interviews/trees/TreeStuffTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class TreeStuffTest
 {
-   /** @formatter:off */
+    /** @formatter:off */
     /**
      *               10
      *              5  15
@@ -16,77 +16,77 @@ public class TreeStuffTest
      *           2
      */
    /** @formatter:on */
-   @Test
-   public void testIsBalancedFalse()
-   {
-      // root setup
-      TreeNode treeNodeRoot = new TreeNode(new Integer(10));
-      TreeNode leftTreeNode = new TreeNode(new Integer((5)));
-      TreeNode rightTreeNode = new TreeNode(new Integer((15)));
+    @Test
+    public void testIsBalancedFalse()
+    {
+        // root setup
+        TreeNode treeNodeRoot = new TreeNode(new Integer(10));
+        TreeNode leftTreeNode = new TreeNode(new Integer((5)));
+        TreeNode rightTreeNode = new TreeNode(new Integer((15)));
 
-      // set root left and right
-      leftTreeNode.setLeft(new TreeNode(new Integer(4)));
-      leftTreeNode.getLeft().setLeft(new TreeNode(new Integer(3)));
-      leftTreeNode.getLeft().getLeft().setLeft(new TreeNode(new Integer(2)));
-      treeNodeRoot.setLeft(leftTreeNode);
-      treeNodeRoot.setRight(rightTreeNode);
+        // set root left and right
+        leftTreeNode.setLeft(new TreeNode(new Integer(4)));
+        leftTreeNode.getLeft().setLeft(new TreeNode(new Integer(3)));
+        leftTreeNode.getLeft().getLeft().setLeft(new TreeNode(new Integer(2)));
+        treeNodeRoot.setLeft(leftTreeNode);
+        treeNodeRoot.setRight(rightTreeNode);
 
-      boolean isBalanced = TreeStuff.isBalanced(treeNodeRoot);
-      assertFalse(isBalanced);
-   }
+        boolean isBalanced = TreeStuff.isBalanced(treeNodeRoot);
+        assertFalse(isBalanced);
+    }
 
-   /** @formatter:off */
+    /** @formatter:off */
     /**
      *             10
      *           5    15
      *         3  6  11 16
      */
     /** @formatter:on */
-   @Test
-   public void testIsBalancedTrue()
-   {
-      // root setup
-      TreeNode treeNodeRoot = new TreeNode(new Integer(10));
-      TreeNode leftTreeNode = new TreeNode(new Integer((5)));
-      TreeNode rightTreeNode = new TreeNode(new Integer((15)));
+    @Test
+    public void testIsBalancedTrue()
+    {
+        // root setup
+        TreeNode treeNodeRoot = new TreeNode(new Integer(10));
+        TreeNode leftTreeNode = new TreeNode(new Integer((5)));
+        TreeNode rightTreeNode = new TreeNode(new Integer((15)));
 
-      // set root left and right
-      leftTreeNode.setLeft(new TreeNode(new Integer(3)));
-      leftTreeNode.setRight(new TreeNode(new Integer(6)));
-      rightTreeNode.setLeft(new TreeNode(new Integer(11)));
-      rightTreeNode.setRight(new TreeNode(new Integer(16)));
-      treeNodeRoot.setLeft(leftTreeNode);
-      treeNodeRoot.setRight(rightTreeNode);
+        // set root left and right
+        leftTreeNode.setLeft(new TreeNode(new Integer(3)));
+        leftTreeNode.setRight(new TreeNode(new Integer(6)));
+        rightTreeNode.setLeft(new TreeNode(new Integer(11)));
+        rightTreeNode.setRight(new TreeNode(new Integer(16)));
+        treeNodeRoot.setLeft(leftTreeNode);
+        treeNodeRoot.setRight(rightTreeNode);
 
-      boolean isBalanced = TreeStuff.isBalanced(treeNodeRoot);
-      assertTrue(isBalanced);
-   }
+        boolean isBalanced = TreeStuff.isBalanced(treeNodeRoot);
+        assertTrue(isBalanced);
+    }
 
-   /** @formatter:off */
+    /** @formatter:off */
    /**
     *             10
     *           5    15
     *         3  6  11 16
     */
    /** @formatter:on */
-   @Test
-   public void testMaxValue()
-   {
-      // root setup
-      TreeNode treeNodeRoot = new TreeNode(new Integer(10));
-      TreeNode leftTreeNode = new TreeNode(new Integer((5)));
-      TreeNode rightTreeNode = new TreeNode(new Integer((15)));
+    @Test
+    public void testMaxValue()
+    {
+        // root setup
+        TreeNode treeNodeRoot = new TreeNode(new Integer(10));
+        TreeNode leftTreeNode = new TreeNode(new Integer((5)));
+        TreeNode rightTreeNode = new TreeNode(new Integer((15)));
 
-      // set root left and right
-      leftTreeNode.setLeft(new TreeNode(new Integer(3)));
-      leftTreeNode.setRight(new TreeNode(new Integer(6)));
-      rightTreeNode.setLeft(new TreeNode(new Integer(11)));
-      rightTreeNode.setRight(new TreeNode(new Integer(16)));
-      treeNodeRoot.setLeft(leftTreeNode);
-      treeNodeRoot.setRight(rightTreeNode);
+        // set root left and right
+        leftTreeNode.setLeft(new TreeNode(new Integer(3)));
+        leftTreeNode.setRight(new TreeNode(new Integer(6)));
+        rightTreeNode.setLeft(new TreeNode(new Integer(11)));
+        rightTreeNode.setRight(new TreeNode(new Integer(16)));
+        treeNodeRoot.setLeft(leftTreeNode);
+        treeNodeRoot.setRight(rightTreeNode);
 
-      Integer maxTraversal = TreeStuff.maxValue(treeNodeRoot);
-      System.out.println(maxTraversal);
-   }
+        Integer maxTraversal = TreeStuff.maxValue(treeNodeRoot);
+        System.out.println(maxTraversal);
+    }
 
 }
